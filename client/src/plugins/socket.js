faa9e6f081b1f86f1df990122bb0e0a2f337ec01
Navicstein Rotciv <navicsteinrotciv@gmail.com>
2019-07-18 02:50:52 +0100

    changing origin 02

--- client/src/plugins/socket.js
+++ client/src/plugins/socket.js
@@ -17,7 +17,7 @@ io.sails.environment = process.env.NODE_ENV;
 io.sails.url =
   process.env.NODE_ENV == "development"
     ? "//192.168.43.113:1337"
-    : "https://pentecust.herokuapp.com/#/";
+    : "https://pentecust.herokuapp.com";
 io.sails.transports = ["polling"];
 // io.sails.initialConnectionHeaders = { nosession: true };
 // io.socket.on("connect", c => console.clear());
